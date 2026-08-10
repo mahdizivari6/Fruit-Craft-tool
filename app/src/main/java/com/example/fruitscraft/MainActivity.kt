d147ddab8fba91625d5114ffafab3953969a160e
mahdizivari6 <mahdizivari6@gmail.com>
2026-08-10 09:42:28 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/fruitscraft/MainActivity.kt
+++ app/src/main/java/com/example/fruitscraft/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.fruitscraft
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Fruit Craft Tool"
+        text.textSize = 30f
+
+        setContentView(text)
     }
 }
